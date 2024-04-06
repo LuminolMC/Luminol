dddb28fdf6970f48c2deac6d04c20822723121f1
ShintoKosei <67397639+ShintoKosei@users.noreply.github.com>
2024-04-06 21:28:13 +0800

    Jump Build To Debug CI <1>

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -2,15 +2,15 @@ prop() {
   grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
 }
 
-commitid="$(git log --pretty='%h' -1)"
-mcversion="$(prop mcVersion)"
+commitid=$(git log --pretty='%h' -1)
+mcversion=$(prop mcVersion)
 preVersion=$(prop preVersion)
 release_tag="$mcversion-$commitid"
 jarName="build/libs/luminol-1.20.4-paperclip.jar"
 make_latest=$([ $preVersion = "true" ] && echo "false" || echo "true")
 
 echo "commitid=$commitid" >> $GITHUB_ENV
-echo "version=$mcversion" >> $GITHUB_ENV
+echo "mcversion=$mcversion" >> $GITHUB_ENV
 echo "$mcversion"
 echo "tag=$release_tag" >> $GITHUB_ENV
 echo "jar=$jarName" >> $GITHUB_ENV
