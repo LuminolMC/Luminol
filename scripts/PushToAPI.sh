a9238f8e8ed9033d8b99072e9c56f76d1f274fe5
ShintoKosei <67397639+ShintoKosei@users.noreply.github.com>
2024-04-06 20:29:43 +0800

    Update CI

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -7,21 +7,21 @@ prop() {
 }
 echo "$tag"
 project_id="luminol"
-project_name="luminol"
+mcversion_group=$(prop mcVersion_Group)
 mcversion=$(prop mcVersion)
-ctime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+echo "$mcversion_group - $mcversion"
 pre=$(prop preVersion)
 if [ $pre = "true" ]; then
   channel="experimental"
 else
   channel="default"
 fi
-promoted=false
-
-number=$(git log --oneline ver/1.20.4 ^`git describe --tags --abbrev=0` | wc -l)
+number=$(git log --oneline "ver/1.20.4" ^$(git describe --tags --abbrev=0) | wc -l)
 changes=$(git log --pretty='%H<<<%s>>>' -"$number" | sed ':a;N;$!ba;s/\n//g')
-jar_name="build/libs/luminol-1.20.4-paperclip.jar"
-jar_sha256=`sha256 $jar_name`
-
+jar_sha256=`sha256 build/libs/luminol-1.20.4-paperclip.jar`
+echo "$har_sha256"
+jar_name="luminol-1.20.4-paperclip.jar"
+ctime=$(date -u +"%s")"000"
+echo "$ctime"
 # v2
-curl --location --request POST "https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit" --header "Content-Type: application/json" --header "Authentication: $secret_v2" --data-raw "{\"project_id\":\"$project_id\",\"version\":\"$mcversion\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"tag\":\"$tag\"}"
+curl --location --request POST "https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit" --header "Content-Type: application/json" --header "Authentication: $secret_v2" --data-raw "{\"version_group\":\"$mcversion_group\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"release_tag\":\"$tag\",\"time\":\"$ctime\"}"
