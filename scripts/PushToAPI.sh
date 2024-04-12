4aaa3151a277ecdb4ec4ba7d023aeec30123ce4c
Klop233 <3337913379@qq.com>
2024-04-12 22:10:55 +0800

    Update CI

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -23,4 +23,4 @@ ctime=$(date -u +"%s")"000"
 echo "Authentication: $secret_v2"
 echo "[DEBUG] curl --location -g --request POST "https://api.luminolmc.com/v2/projects/luminol/1.20.4/build/commit?jar_name=luminol-1.20.4-paperclip.jar&release_tag=$tag&changes="$changes"&sha256=$jar_sha256&time=$ctime&channel=$channel&version_group=$mcversion_group" -H "Authorization: $secret_v2""
 # shellcheck disable=SC2031
-curl --location -g --request POST "https://api.luminolmc.com/v2/projects/luminol/1.20.4/build/commit?jar_name=luminol-1.20.4-paperclip.jar&release_tag=$tag&changes=$changes&sha256=$jar_sha256&time=$ctime&channel=$channel&version_group=$mcversion_group" -H "Authorization: $secret_v2"
+curl --location -g --request POST -H "Authorization: $secret_v2" "https://api.luminolmc.com/v2/projects/luminol/1.20.4/build/commit?jar_name=$jar_name&release_tag=$tag&changes=$changes&sha256=$jar_sha256&time=$ctime&channel=$channel&version_group=$mcversion_group"
