73f8597df2f99e376d4303f40ba3c6fadcd6ab4e
Klop233 <3337913379@qq.com>
2024-04-13 09:27:47 +0800

    update

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -26,6 +26,10 @@ echo "Authentication: $secret_v2"
 #echo "{\"version_group\":\"$mcversion_group\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"release_tag\":\"$tag\",\"time\":\"$ctime\"}" > data.json
 json=$(printf "{\"channel\":\"%s\", \"jar_name\":\"%s\", \"sha256\":\"%s\", \"tag\":\"%s\", \"time\":\"%s\", \"changes\":\"%s\"}" "$channel" "$jar_name" "$jar_sha256" "$tag" "$ctime" "$changes")
 echo "[DEBUG] $json"
+echo "[DEBUG] curl --location --request POST \"https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit\" \
+    -H \"Content-Type: application/json\" \
+    -H \"Authorization: $secret_v2\" \
+    -d \"$json\""
 response=$(curl --location --request POST "https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit" \
     -H "Content-Type: application/json" \
     -H "Authorization: $secret_v2" \
