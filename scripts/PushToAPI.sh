c790b7b1ca4b684ea2ab8c63f113439a21f375f2
Klop233 <3337913379@qq.com>
2024-04-13 16:43:29 +0800

    绝对搞定

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -34,6 +34,7 @@ echo "[DEBUG] curl --location --request POST \"https://api.luminolmc.com/v2/proj
     --data-urlencode \"jar_name=$jar_name\" \
     --data-urlencode \"sha256=$jar_sha256\" \
     --data-urlencode \"tag=$tag\" \
+    --data-urlencode \"time=$ctime\" \
     --data-urlencode \"changes=$changes\""
 response=$(curl -L --request POST "https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit" \
     -H "Content-Type: application/x-www-form-urlencoded" \
