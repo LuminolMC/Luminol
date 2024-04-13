fd780f417bd55ed8aaf45885e5c4ef73119dd69c
Klop233 <3337913379@qq.com>
2024-04-13 17:20:03 +0800

    Fixed CI

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -5,22 +5,43 @@ sha256() {
 prop() {
   grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
 }
+
 project_id="luminol"
 mcversion_group=$(prop GroupMCV)
 mcversion=$(prop mcVersion)
 pre=$(prop preVersion)
+
 if [ $pre = "true" ]; then
   channel="experimental"
 else
   channel="default"
 fi
-changes=$(git log -1 --pretty='[{"commit": "%H", "message": "%s", "summary": "%b"}]')
+changes=$(git log -1 --pretty="[{\\\"commit\\\": \\\"%H\\\", \\\"message\\\": \\\"%s\\\", \\\"summary\\\": \\\"%s\\\"}]")
 jar_sha256=`sha256 build/libs/luminol-1.20.4-paperclip.jar`
 jar_name="luminol-1.20.4-paperclip.jar"
 ctime=$(date -u +"%s")"000"
 
 # v2
 echo "Authentication: $secret_v2"
-echo "[DEBUG] curl --location -g --request POST "https://api.luminolmc.com/v2/projects/luminol/1.20.4/build/commit?jar_name=luminol-1.20.4-paperclip.jar&release_tag=$tag&changes="$changes"&sha256=$jar_sha256&time=$ctime&channel=$channel&version_group=$mcversion_group" -H "Authorization: $secret_v2""
-# shellcheck disable=SC2031
-curl --location -g --request POST "https://api.luminolmc.com/v2/projects/luminol/1.20.4/build/commit?jar_name=luminol-1.20.4-paperclip.jar&release_tag=$tag&changes=$changes&sha256=$jar_sha256&time=$ctime&channel=$channel&version_group=$mcversion_group" -H "Authorization: $secret_v2"
+#echo "{\"version_group\":\"$mcversion_group\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"release_tag\":\"$tag\",\"time\":\"$ctime\"}" > data.json
+json=$(printf "{\\\"channel\\\":\\\"%s\\\", \\\"jar_name\\\":\\\"%s\\\", \\\"sha256\\\":\\\"%s\\\", \\\"tag\\\":\\\"%s\\\", \\\"time\\\":\\\"%s\\\", \\\"changes\\\":\\\"%s\\\"}" \
+    "$channel" "$jar_name" "$jar_sha256" "$tag" "$ctime" "$changes")
+echo "[DEBUG] $json"
+echo "[DEBUG] curl --location --request POST \"https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit\" \
+    -H \"Content-Type: application/x-www-form-urlencoded\" \
+    -H \"Authorization: $secret_v2\" \
+    --data-urlencode \"channel=$channel\" \
+    --data-urlencode \"jar_name=$jar_name\" \
+    --data-urlencode \"sha256=$jar_sha256\" \
+    --data-urlencode \"release_tag=$tag\" \
+    --data-urlencode \"time=$ctime\" \
+    --data-urlencode \"changes=$changes\""
+curl -L --request POST "https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit" \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -H "Authorization: $secret_v2" \
+    --data-urlencode "channel=$channel" \
+    --data-urlencode "jar_name=$jar_name" \
+    --data-urlencode "sha256=$jar_sha256" \
+    --data-urlencode "release_tag=$tag" \
+    --data-urlencode "time=$ctime" \
+    --data-urlencode "changes=$changes"
