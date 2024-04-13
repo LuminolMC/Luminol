850c60d04c30710cb0204f2b024692db19bb915b
ShintoKosei <2081384668@qq.com>
2024-04-14 07:09:19 +0800

    update CI

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -1,7 +1,6 @@
 sha256() {
   sha256sum $1 | awk '{print $1}'
 }
-
 prop() {
   grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
 }
@@ -16,33 +15,11 @@ if [ $pre = "true" ]; then
 else
   channel="default"
 fi
+
 changes=$(git log -1 --pretty="[{\"commit\": \"%H\", \"message\": \"%s\", \"summary\": \"%s\"}]")
 jar_sha256=`sha256 build/libs/luminol-1.20.4-paperclip.jar`
 jar_name="luminol-1.20.4-paperclip.jar"
 ctime=$(date -u +"%s")"000"
 
 # v2
-echo "Authentication: $secret_v2"
-#echo "{\"version_group\":\"$mcversion_group\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"release_tag\":\"$tag\",\"time\":\"$ctime\"}" > data.json
-json=$(printf "{\\\"channel\\\":\\\"%s\\\", \\\"jar_name\\\":\\\"%s\\\", \\\"sha256\\\":\\\"%s\\\", \\\"tag\\\":\\\"%s\\\", \\\"time\\\":\\\"%s\\\", \\\"changes\\\":\\\"%s\\\"}" \
-    "$channel" "$jar_name" "$jar_sha256" "$tag" "$ctime" "$changes")
-echo "[DEBUG] $json"
-echo "[DEBUG] $changes"
-echo "[DEBUG] curl --location --request POST \"https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit\" \
-    -H \"Content-Type: application/x-www-form-urlencoded\" \
-    -H \"Authorization: $secret_v2\" \
-    --data-urlencode \"channel=$channel\" \
-    --data-urlencode \"jar_name=$jar_name\" \
-    --data-urlencode \"sha256=$jar_sha256\" \
-    --data-urlencode \"release_tag=$tag\" \
-    --data-urlencode \"time=$ctime\" \
-    --data-urlencode \"changes=$changes\""
-curl -L --request POST "https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit" \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -H "Authorization: $secret_v2" \
-    --data-urlencode "channel=$channel" \
-    --data-urlencode "jar_name=$jar_name" \
-    --data-urlencode "sha256=$jar_sha256" \
-    --data-urlencode "release_tag=$tag" \
-    --data-urlencode "time=$ctime" \
-    --data-urlencode "changes=$changes"
+curl -L --request POST "https://api.luminolmc.com/v2/projects/$project_id/$mcversion/build/commit" -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: $secret_v2" --data-urlencode "channel=$channel" --data-urlencode "jar_name=$jar_name" --data-urlencode "sha256=$jar_sha256" --data-urlencode "release_tag=$tag" --data-urlencode "time=$ctime" --data-urlencode "changes=$changes"
--- scripts/SetENV.sh
+++ scripts/SetENV.sh
@@ -11,7 +11,6 @@ make_latest=$([ $preVersion = "true" ] && echo "false" || echo "true")
 
 echo "commitid=$commitid" >> $GITHUB_ENV
 echo "mcversion=$mcversion" >> $GITHUB_ENV
-echo "$mcversion"
 echo "tag=$release_tag" >> $GITHUB_ENV
 echo "jar=$jarName" >> $GITHUB_ENV
 echo "pre=$preVersion" >> $GITHUB_ENV
