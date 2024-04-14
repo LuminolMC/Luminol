38a8161c1a00d0e599eeb714fb6d3c2703d9b230
ShintoKosei <2081384668@qq.com>
2024-04-14 12:11:05 +0800

    Update CI

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -1,29 +1,27 @@
 sha256() {
   sha256sum $1 | awk '{print $1}'
 }
-
 prop() {
-  grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
+  grep "^[[:space:]]*${1}" gradle.properties | cut -d'=' -f2 | sed 's/^[[:space:]]*//; s/\r//'
 }
 
-project_id="luminol"
 mcversion_group=$(prop GroupMCV)
 mcversion=$(prop mcVersion)
 pre=$(prop preVersion)
+pro_id_mcv=$project_id-$mcversion
 
 if [ $pre = "true" ]; then
   channel="experimental"
 else
   channel="default"
 fi
+
 changes=$(git log -1 --pretty="[{\"commit\": \"%H\", \"message\": \"%s\", \"summary\": \"%s\"}]")
-jar_sha256=`sha256 build/libs/luminol-1.20.4-paperclip.jar`
-jar_name="luminol-1.20.4-paperclip.jar"
+jar_sha256=`sha256 build/libs/$pro_id_mcv-paperclip.jar`
+jar_name="$pro_id_mcv-paperclip.jar"
 ctime=$(date -u +"%s")"000"
 
 # v2
-echo "Authentication: $secret_v2"
-#echo "{\"version_group\":\"$mcversion_group\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"release_tag\":\"$tag\",\"time\":\"$ctime\"}" > data.json
 json=$(printf "{\\\"channel\\\":\\\"%s\\\", \\\"jar_name\\\":\\\"%s\\\", \\\"sha256\\\":\\\"%s\\\", \\\"tag\\\":\\\"%s\\\", \\\"time\\\":\\\"%s\\\", \\\"changes\\\":\\\"%s\\\"}" \
     "$channel" "$jar_name" "$jar_sha256" "$tag" "$ctime" "$changes")
 echo "[DEBUG] $json"
