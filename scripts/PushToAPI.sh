0811e8d76d438bc0dda6bc8b36d6215becc59def
ShintoKosei <2081384668@qq.com>
2024-04-14 08:55:38 +0800

    update ci

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -5,10 +5,10 @@ prop() {
   grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
 }
 
-project_id="luminol"
 mcversion_group=$(prop GroupMCV)
 mcversion=$(prop mcVersion)
 pre=$(prop preVersion)
+pro_id_mcv=$project_id-$mcversion
 
 if [ $pre = "true" ]; then
   channel="experimental"
@@ -17,8 +17,8 @@ else
 fi
 
 changes=$(git log -1 --pretty="[{\"commit\": \"%H\", \"message\": \"%s\", \"summary\": \"%s\"}]")
-jar_sha256=`sha256 build/libs/luminol-1.20.4-paperclip.jar`
-jar_name="luminol-1.20.4-paperclip.jar"
+jar_sha256=`sha256 build/libs/$pro_id_mcv-paperclip.jar`
+jar_name="$pro_id_mcv-paperclip.jar"
 ctime=$(date -u +"%s")"000"
 
 # v2
--- scripts/SetENV.sh
+++ scripts/SetENV.sh
@@ -2,16 +2,29 @@ prop() {
   grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
 }
 
+project_id_b="Luminol"
+project_id="luminol"
+
 commitid=$(git log --pretty='%h' -1)
 mcversion=$(prop mcVersion)
+grdversion=$(prop version)
 preVersion=$(prop preVersion)
 release_tag="$mcversion-$commitid"
-jarName="build/libs/luminol-1.20.4-paperclip.jar"
+jarName="$project_id-$mcversion-paperclip.jar"
+jarName_dir="build/libs/$jarName"
 make_latest=$([ $preVersion = "true" ] && echo "false" || echo "true")
 
-echo "commitid=$commitid" >> $GITHUB_ENV
+mv build/libs/$project_id-paperclip-$grdversion-reobf.jar build/libs/$jarName
+mv build/libs/$project_id-bundler-$grdversion-reobf.jar build/libs/$project_id-$mcversion-bundler.jar
+
+echo "project_id=$project_id" >> $GITHUB_ENV
+echo "project_id_b=$project_id_b" >> $GITHUB_ENV
+echo "commit_id=$commitid" >> $GITHUB_ENV
+echo "commit_msg=$(git log --pretty='> [%h] %s' -$number)" >> $GITHUB_ENV
+echo "$commit_msg"
 echo "mcversion=$mcversion" >> $GITHUB_ENV
+echo "pre=$preVersion" >> $GITHUB_ENV
 echo "tag=$release_tag" >> $GITHUB_ENV
 echo "jar=$jarName" >> $GITHUB_ENV
-echo "pre=$preVersion" >> $GITHUB_ENV
+echo "jar_dir=$jarName_dir" >> $GITHUB_ENV
 echo "make_latest=$make_latest" >> $GITHUB_ENV
