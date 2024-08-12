0486f22863fe52a8c7c38b71ec7595ddb0ff2def
M2ke4U <79621885+MrHua269@users.noreply.github.com>
2024-08-12 19:03:08 +0800

    [ci skip]Remove incorrect env init

--- scripts/PushToMaven.sh
+++ scripts/PushToMaven.sh
@@ -1,8 +1,4 @@
 prop() {
   grep "^[[:space:]]*${1}" gradle.properties | cut -d'=' -f2 | sed 's/^[[:space:]]*//; s/\r//'
 }
-
-MAVEN_REPO_USER=${{ secrets.MAVEN_REPO_USER }}
-MAVEN_REPO_PASSWORD=${{ secrets.MAVEN_REPO_PASSWORD }}
-
 sh publish -PpublishDevBundle=true
